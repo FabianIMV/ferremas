3d07b9ebad08729aaa16efa4c9407a7c8da19b48
FabianIMV <fabianignaciomv@gmail.com>
2024-04-10 12:11:41 -0400

    logo

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,7 +4,7 @@ import logo from './logoferremas.svg'
 
 class NavBar extends Component {
   render() {
-    return <div className="navbar"> 
+    return <div className="navbar">  
     <img src={logo} alt="Logo" className="logo"/>
     <button className="btn btn-primary btn-block mb-4 iniciar-sesion">Iniciar Sesión</button>
     </div>;
