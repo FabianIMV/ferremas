a27b0826c236f068a38502423be0ff68fbf69708
FabianIMV <fabianignaciomv@gmail.com>
2024-04-10 14:13:30 -0400

    Se actualiza search bar

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-      <NavBar />
+        <NavBar />
+      <div className="search-bar">
+        <div className="input-group mb-3">
+          <input type="text" className="form-control" placeholder="Buscar productos..." aria-label="Buscar" aria-describedby="button-addon2"></input>
+          <div class="btn btn-outline-secondary" type="button" id="button-addon2">Buscar</div>
+        </div>
+      </div>
     </div>
   );
 }
