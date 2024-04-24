b0e78ca2c98648f8029e92f94cddba15f81d0fa4
FabianIMV <fabianignaciomv@gmail.com>
2024-04-24 18:28:33 -0400

    se crea authcontext para manejo de estado de sesion
    se crea authcontext para manejo de estado de sesion

--- src/AuthContext.js
+++ src/AuthContext.js
@@ -1,11 +1,10 @@
 import { createContext, useState } from 'react';
-import { CognitoUserPool } from 'amazon-cognito-identity-js';
+import { CognitoUserPool, AuthenticationDetails, CognitoUser } from 'amazon-cognito-identity-js';
 
 const poolData = {
   UserPoolId: 'us-east-1_UIO4gNUY8',
   ClientId: '3h1acnaqqioq9kf08d6t0tico1'
 };
-
 
 const userPool = new CognitoUserPool(poolData);
 
@@ -13,6 +12,24 @@ export const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const login = (username, password) => {
+    const authenticationDetails = new AuthenticationDetails({
+      Username: username,
+      Password: password
+    });
+
+    const cognitoUser = new CognitoUser({ Username: username, Pool: userPool });
+
+    cognitoUser.authenticateUser(authenticationDetails, {
+      onSuccess: () => {
+        setIsAuthenticated(true);
+      },
+      onFailure: err => {
+        console.error(err);
+      }
+    });
+  };
 
   const logout = () => {
     const cognitoUser = userPool.getCurrentUser();
@@ -25,7 +42,7 @@ export function AuthProvider({ children }) {
   };
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated, logout }}>
+    <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
--- src/components/SignUp/Signup.js
+++ src/components/SignUp/Signup.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Signup.css';
-import { CognitoUserPool } from 'amazon-cognito-identity-js';
+import { CognitoUserPool, CognitoUser } from 'amazon-cognito-identity-js';
 
 const Signup = () => {
     const navigate = useNavigate();
@@ -11,6 +11,8 @@ const Signup = () => {
     const [confirmPassword, setConfirmPassword] = useState('');
     const [email, setEmail] = useState('');
     const [message, setMessage] = useState('');
+    const [confirmationCode, setConfirmationCode] = useState('');
+    const [showConfirmationForm, setShowConfirmationForm] = useState(false);
 
     const poolData = {
         UserPoolId: 'us-east-1_UIO4gNUY8',
@@ -32,7 +34,25 @@ const Signup = () => {
                 setErrorMessage(err.message || 'Error al registrar usuario.');
                 return;
             }
-            setMessage('Usuario registrado con éxito.');
+            setMessage('Usuario registrado con éxito. Por favor, confirma tu registro.');
+            setShowConfirmationForm(true);
+        });
+    };
+
+    const handleConfirmation = async (event) => {
+        event.preventDefault();
+
+        const cognitoUser = new CognitoUser({
+            Username: username,
+            Pool: userPool
+        });
+
+        cognitoUser.confirmRegistration(confirmationCode, true, function(err, result) {
+            if (err) {
+                alert(err.message || JSON.stringify(err));
+                return;
+            }
+            console.log('call result: ' + result);
             navigate(`/Bienvenido/${username}`);
         });
     };
@@ -41,25 +61,36 @@ const Signup = () => {
         <div>
         <div className="signup-container">
             <h2>Registrarse</h2>
-            <form onSubmit={handleSubmit} className="signup-form">
-                <label>
-                    Nombre de Usuario:
-                    <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="form-control" />
-                </label>
-                <label>
-                    Email:
-                    <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} className="form-control" />
-                </label>
-                <label>
-                    Contraseña:
-                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="form-control" />
-                </label>
-                <label>
-                    Confirmar Contraseña:
-                    <input type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} className="form-control" />
-                </label>
-                <input type="submit" value="Registrarse" className="btn btn-primary bnt-block mb-4 iniciar-sesion" />
-            </form>
+            {!showConfirmationForm && (
+                <form onSubmit={handleSubmit} className="signup-form">
+                    <label>
+                        Nombre de Usuario:
+                        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="form-control" />
+                    </label>
+                    <label>
+                        Email:
+                        <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} className="form-control" />
+                    </label>
+                    <label>
+                        Contraseña:
+                        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="form-control" />
+                    </label>
+                    <label>
+                        Confirmar Contraseña:
+                        <input type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} className="form-control" />
+                    </label>
+                    <input type="submit" value="Registrarse" className="btn btn-primary bnt-block mb-4 iniciar-sesion" />
+                </form>
+            )}
+            {showConfirmationForm && (
+                <form onSubmit={handleConfirmation} className="signup-form">
+                    <label>
+                        Código de confirmación:
+                        <input type="text" value={confirmationCode} onChange={(e) => setConfirmationCode(e.target.value)} className="form-control" />
+                    </label>
+                    <input type="submit" value="Confirmar Registro" className="btn btn-primary bnt-block mb-4 iniciar-sesion" />
+                </form>
+            )}
         </div>
         {errorMessage && <p>{errorMessage}</p>}
         {message && <p>{message}</p>}
