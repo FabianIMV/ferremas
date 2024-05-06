5826aacc9d202701eb3964ec17fcf5b5b7e08784
FabianIMV <fabianignaciomv@gmail.com>
2024-05-06 11:06:21 -0400

    Se integra api de cmf para cambio de dolar

--- src/components/ProductTable/ProductTable.js
+++ src/components/ProductTable/ProductTable.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState} from 'react';
 import alicate from './alicate.webp';
 import serrucho from './serrucho.webp';
 import destornillador from './destornillador.webp';
@@ -13,9 +13,54 @@ import tornillomadera from './tornillomadera.webp';
 import tornillotecho from './tornillotecho.webp';
 import item from './item.png';
 import '../../App.css';
+import axios from 'axios';
+import xml2js from 'xml2js';
 
+const images = {
+  alicate,
+  serrucho,
+  'lentes de seguridad': lente,
+  'overol de trabajo': overol,
+  destornillador,
+  'casco de seguridad': casco,
+  cemento,
+  mortero,
+  'plancha de zinc': plancha,
+  'tornillo autoperforante': tornilloautoperforante,
+  'tornillo madera': tornillomadera,
+  'tornillo techo': tornillotecho,
+  default: item,
+};
 
 function ProductTable({ products, addToCart }) {
+  const [exchangeRate, setExchangeRate] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    axios.get('https://api.cmfchile.cl/api-sbifv3/recursos_api/dolar?apikey=ab7f92c29c235cc96ef34099b8ba9cea5731ad2a&formato=xml')
+    .then(response => {
+      xml2js.parseString(response.data, (err, result) => {
+        if (err) {
+          console.error('Error parsing XML', err);
+          setError('Error parsing XML');
+        } else {
+          console.log(result)
+          const rate = parseFloat(result.IndicadoresFinancieros.Dolares[0].Dolar[0].Valor[0].replace(',', '.'));
+          setExchangeRate(rate);
+          console.log(rate);
+        }
+      });
+    })
+    .catch(error => {
+      console.error('Error fetching exchange rate.', error);
+      setError('Error fetching exchange rate.');
+    });
+  }, []);
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
   if (products.length === 0) {
     return null;
   }
@@ -30,56 +75,26 @@ function ProductTable({ products, addToCart }) {
       {productRows.map((productRow, rowIndex) => (
         <div key={rowIndex} className="row">
           {productRow.map((product) => {
-            let imageSrc;
-          if (product.name.toLowerCase() === 'alicate') {
-            imageSrc = alicate;
-          } else if (product.name.toLowerCase() === 'serrucho') {
-            imageSrc = serrucho;
-          } else if (product.name.toLowerCase() === 'lentes de seguridad') {
-            imageSrc = lente;
-          } else if (product.name.toLowerCase() === 'overol de trabajo') {
-            imageSrc = overol;
-          } else if (product.name.toLowerCase() === 'destornillador') {
-            imageSrc = destornillador;
-          } else if (product.name.toLowerCase() === 'casco de seguridad') {
-            imageSrc = casco;
-          } else if (product.name.toLowerCase() === 'overol de trabajo') {
-            imageSrc = overol;
-          } else if (product.name.toLowerCase() === 'cemento') {
-            imageSrc = cemento;
-          } else if (product.name.toLowerCase() === 'mortero') {
-            imageSrc = mortero;
-          } else if (product.name.toLowerCase() === 'plancha de zinc') {
-            imageSrc = plancha;
-          } else if (product.name.toLowerCase() === 'tornillo autoperforante') {
-            imageSrc = tornilloautoperforante;
-          } else if (product.name.toLowerCase() === 'tornillo madera') {
-            imageSrc = tornillomadera;
-          } else if (product.name.toLowerCase() === 'tornillo techo') {
-            imageSrc = tornillotecho;
-          } else {
-            imageSrc = item;
-          }
+            const imageSrc = images[product.name.toLowerCase()] || images.default;
 
-
-          return (
-            <div key={product.name} className="col" style={{ padding: '10px' }}>
-              <div className="card" style={{ width: '100%', height: '100%' }}>
-                <img src={imageSrc} alt="item" className="card-img-top"></img>
-                <div className="card-body">
-                  <h5 className="card-title">{product.name}</h5>
-                  <p className="card-text">${product.price} - US {product.price}</p>
-                  <button className="btn btn-outline-secondary" onClick={() => addToCart(product)}>Agregar al carrito</button>
-                  <button className="btn btn-outline-secondary">Comprar ahora</button>
+            return (
+              <div key={product.name} className="col" style={{ padding: '10px' }}>
+                <div className="card" style={{ width: '100%', height: '100%' }}>
+                  <img src={imageSrc} alt="item" className="card-img-top"></img>
+                  <div className="card-body">
+                    <h5 className="card-title">{product.name}</h5>
+                    <p className="card-text">{exchangeRate && `$${Math.round(product.price * exchangeRate)}`} - USD {product.price} </p>                  
+                    <button className="btn btn-outline-secondary" onClick={() => addToCart(product)}>Agregar al carrito</button>
+                    <button className="btn btn-outline-secondary">Comprar ahora</button>
+                  </div>
                 </div>
               </div>
-            </div>
-          );
-        })}
-      </div>
-    ))}
-  </div>
-);
+            );
+          })}
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default ProductTable;
