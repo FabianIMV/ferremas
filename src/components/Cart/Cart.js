1dea52d0ad0525bd2d5226ea99d407bdd9eb1d8a
FabianIMV <fabianignaciomv@gmail.com>
2024-05-07 10:01:55 -0400

    se mejora logica del carrito

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,6 +1,6 @@
 import React, { useContext, useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import {CartContext}  from './CartContext';
+import { CartContext } from './CartContext';
 import './Cart.css';
 import axios from 'axios';
 import xml2js from 'xml2js';
@@ -12,19 +12,19 @@ const Cart = ({ isDropdown, setIsDropdown }) => {
 
     useEffect(() => {
         axios.get('https://api.cmfchile.cl/api-sbifv3/recursos_api/dolar?apikey=ab7f92c29c235cc96ef34099b8ba9cea5731ad2a&formato=xml')
-        .then(response => {
-            xml2js.parseString(response.data, (err, result) => {
-                if (err) {
-                    console.error('Error parsing XML', err);
-                } else {
-                    const rate = parseFloat(result.IndicadoresFinancieros.Dolares[0].Dolar[0].Valor[0].replace(',', '.'));
-                    setExchangeRate(rate);
-                }
+            .then(response => {
+                xml2js.parseString(response.data, (err, result) => {
+                    if (err) {
+                        console.error('Error parsing XML', err);
+                    } else {
+                        const rate = parseFloat(result.IndicadoresFinancieros.Dolares[0].Dolar[0].Valor[0].replace(',', '.'));
+                        setExchangeRate(rate);
+                    }
+                });
+            })
+            .catch(error => {
+                console.error('Error fetching exchange rate.', error);
             });
-        })
-        .catch(error => {
-            console.error('Error fetching exchange rate.', error);
-        });
     }, []);
 
     const addToCart = (productToAdd) => {
@@ -35,12 +35,12 @@ const Cart = ({ isDropdown, setIsDropdown }) => {
             setCart(prevCart => [...prevCart, { ...productToAdd, quantity: 1, totalPrice: productToAdd.price }]);
         }
     };
-    
+
     const removeFromCart = (productToRemove) => {
         const updatedCart = cart.filter(product => product.name !== productToRemove.name);
         setCart(updatedCart);
     };
-    
+
     const increaseQuantity = (productToIncrease) => {
         const updatedCart = cart.map(product =>
             product.name === productToIncrease.name
@@ -49,14 +49,18 @@ const Cart = ({ isDropdown, setIsDropdown }) => {
         );
         setCart(updatedCart);
     };
-    
+
     const decreaseQuantity = (productToDecrease) => {
-        const updatedCart = cart.map(product =>
-            product.name === productToDecrease.name
-                ? { ...product, quantity: product.quantity > 1 ? product.quantity - 1 : 1, totalPrice: (product.quantity > 1 ? product.quantity - 1 : 1) * product.price }
-                : product
-        );
-        setCart(updatedCart);
+        if (productToDecrease.quantity === 1) {
+            removeFromCart(productToDecrease);
+        } else {
+            const updatedCart = cart.map(product =>
+                product.name === productToDecrease.name
+                    ? { ...product, quantity: product.quantity - 1, totalPrice: (product.quantity - 1) * product.price }
+                    : product
+            );
+            setCart(updatedCart);
+        }
     };
 
     const cleanCart = () => {
@@ -82,29 +86,28 @@ const Cart = ({ isDropdown, setIsDropdown }) => {
                     {cart.map((product, index) => (
                         <div key={index} className="cart-item-component">
                             <p>{product.name}</p>
-                            <p>Total: ${product.totalPrice} - USD ${exchangeRate ? Math.round(product.totalPrice / exchangeRate) : 'Cargando...'}</p>
-                            <button onClick={() => removeFromCart(product)} className="btn btn-danger">Eliminar</button>
                             <button onClick={() => decreaseQuantity(product)} className="btn btn-secondary">-</button>
                             <p>{product.quantity}</p>
                             <button onClick={() => increaseQuantity(product)} className="btn btn-secondary">+</button>
+                            <p>Total: ${product.totalPrice} - USD ${exchangeRate ? Math.round(product.totalPrice / exchangeRate) : ''}</p>
                         </div>
-                   ))}
-                   {!isDropdown && (
-                       <>
-                           <button onClick={cleanCart} className="btn btn-warning">Vaciar carrito</button>
-                           <button onClick={goToPayment} className="btn btn-success">Pagar carrito</button>
-                       </>
-                   )}
-                   {isDropdown && (
-                       <>
-                          
-                               <button onClick={handleGoToCart} className="btn btn-primary">Ir al carrito</button>
-                  
-                       </>
-                   )}
-               </>
-           )}
-       </div>
-   );
+                    ))}
+                    {!isDropdown && (
+                        <>
+                            <button onClick={cleanCart} className="btn btn-warning">Vaciar carrito</button>
+                            <button onClick={goToPayment} className="btn btn-success">Pagar carrito</button>
+                        </>
+                    )}
+                    {isDropdown && (
+                        <>
+
+                            <button onClick={handleGoToCart} className="btn btn-primary">Ir al carrito</button>
+
+                        </>
+                    )}
+                </>
+            )}
+        </div>
+    );
 };
 export default Cart;
